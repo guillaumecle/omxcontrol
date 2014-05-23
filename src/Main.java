206888ec31b37434008d44e0d77c53f49238c1e7
Clément Guillaume <clementguillaume56@hotmail.fr>
2014-05-23 01:04:03 -0700

    ouput

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		for (String arg : args) {
-			System.out.println(arg);
-		}
+	public static void main(String[] args) throws IOException {
+        Process omx = Runtime.getRuntime().exec("omxplayer");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(omx.getInputStream()));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
 	}
 
 }
