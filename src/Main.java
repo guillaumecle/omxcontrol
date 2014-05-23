cb0275bdacb758ceb6aa597788f0d658173245b9
Clément Guillaume <clementguillaume56@hotmail.fr>
2014-05-23 01:14:19 -0700

    ouput

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,53 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
         Process omx = Runtime.getRuntime().exec("omxplayer ~/music/*");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(omx.getInputStream()));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            System.out.println(line);
-        }
-        System.out.println("omx instance is terminated");
+        new StdReader(omx).start();
+        new ErrReader(omx).start();
     }
 
+    public static class StdReader extends Thread {
+
+        private Process omx;
+
+        public StdReader(Process omx) {
+            this.omx = omx;
+        }
+
+        @Override
+        public void run() {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(omx.getInputStream()));
+            String line;
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("omx instance is terminated");
+        }
+    }
+
+    public static class ErrReader extends Thread {
+
+        private Process omx;
+
+        public ErrReader(Process omx) {
+            this.omx = omx;
+        }
+
+        @Override
+        public void run() {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(omx.getErrorStream()));
+            String line;
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    System.err.println("#" + line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("omx instance is terminated");
+        }
+    }
 }
