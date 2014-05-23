a73becae341fc984b9e7a342123bfe6e42cbd1cb
Clément Guillaume <clementguillaume56@hotmail.fr>
2014-05-23 01:07:54 -0700

    ouput

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-        Process omx = Runtime.getRuntime().exec("omxplayer");
+        Process omx = Runtime.getRuntime().exec("omxplayer ~/music/*");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(omx.getInputStream()));
         String line;
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
         }
-	}
+        System.out.println("omx instance is terminated");
+    }
 
 }
